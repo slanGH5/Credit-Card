039d50ca09b6cec965039f39782c7fd1997c54c5
Alan Le <slan05@gmail.com>
2019-09-12 18:08:19 -0700

    Update CreditCard.h

--- CreditCard.h
+++ CreditCard.h
@@ -1,10 +1,4 @@
-//
-//  CreditCard.h
 //  Project 5
-//
-//  Created by Jessica Chen on 7/29/19.
-//  Copyright © 2019 Jessica Chen. All rights reserved.
-//
 
 #ifndef CreditCard_h
 #define CreditCard_h
