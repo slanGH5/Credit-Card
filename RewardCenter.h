8056124295a994d1bb10def08e7ebfcdea3198d1
Alan Le <slan05@gmail.com>
2019-09-12 18:09:32 -0700

    Update RewardCenter.h

--- RewardCenter.h
+++ RewardCenter.h
@@ -1,10 +1,4 @@
-//
 //  RewardCenter.h .h
-//  Project 5
-//
-//  Created by Jessica Chen on 7/29/19.
-//  Copyright © 2019 Jessica Chen. All rights reserved.
-//
 
 #ifndef RewardCenter_h__h
 #define RewardCenter_h__h
